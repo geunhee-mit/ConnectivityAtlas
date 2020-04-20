a0ad44313dc6a877b08d05d687c65a8bacec9b3c
geunhee <lee.geunheee@gmail.com>
2020-04-19 20:32:10 -0400

    슬픔... 운동이나 하고 오자...

--- js/script.js
+++ js/script.js
@@ -302,3 +302,51 @@ function getScrollbarSize() {
   document.body.removeChild(div);
   return size;
 }
+
+
+///line
+var x = $(".one h1").offset();
+var one = $(".one").width();
+var para = $(".one h1").width();
+var right = one - (x.left + para);
+var twoOffset = $(".two h1").offset();
+var twoLeftOffset = twoOffset.left - one;
+var firstLine = twoLeftOffset + right;
+var leftPos = para + x.left;
+
+var left = one - (x.right + para);
+var twoRightOffset = twoOffset.right - one;
+var rightPos = para + x.right;
+
+$(".horizontal-line").css({"top": x.top, "left": leftPos});
+
+$(".horizontal-line").css({"top": x.top, "right": rightPos});
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+var controller = new ScrollMagic.Controller();
+
+		// define movement of panels
+		var wipeAnimation = new TimelineMax()
+			.to("#slideContainer", 1,   {x: "-75%"})
+
+		// create scene to pin and link animation
+		new ScrollMagic.Scene({
+				triggerElement: "#pinContainer",
+				triggerHook: "onLeave",
+				duration: "500%"
+			})
+		  .setPin("#pinContainer")
+			.setTween(wipeAnimation)
+			.addIndicators()
+			.addTo(controller);
+
+
+   var horizontal = new ScrollMagic.Scene({
+        offset: 50,
+        duration: 300,
+       // reverse: false
+      }).setTween(".horizontal-line", {width: firstLine}) // trigger a TweenMax.to tween
+        // .addIndicators()
+        .addTo(controller);
